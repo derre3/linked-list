397fa6374eee94318c8ffe098ad5233310ba6e96
andre <123985988+derre3@users.noreply.github.com>
2023-11-01 15:02:23 -0300

    add append, head, tail and size methods to linkedList

--- linked-list.js
+++ linked-list.js
@@ -1,5 +1,38 @@
-function linkedList() {}
+function linkedList() {
+  const list = {
+    head: null,
+    tail: null,
+    size: null,
+  };
+
+  const append = (value) => {
+    const node = createNode(value);
+    if (list.size === null) {
+      list.head = node;
+      list.tail = node;
+      list.size = 0;
+    } else {
+      list.tail.next = node;
+      list.tail = node;
+      list.size += 1;
+    }
+  };
+
+  const head = () => list.head;
+  const tail = () => list.tail;
+  const size = () => list.size;
+
+  return { append, head, tail, size };
+}
 
 function createNode(data = null, next = null) {
   return { data, next };
 }
+
+const list = linkedList();
+list.append('a');
+list.append('b');
+list.append('c');
+console.log(list.head().data);
+console.log(list.head().next.data);
+console.log(list.tail().data);
